c88f8e3d9dfb61e228a0e0c721c7b093d2f0e6bf
duckcraft7 <duckcraft210@gmail.com>
2026-01-12 17:13:01 -0300

    Implement start button functionality
    Add event listener to start button for menu interaction

---
+++ js/start.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.getElementById('menu')
+  const startBtn = document.getElementById('startGame')
+
+  startBtn.onclick = () => {
+    // some com o menu
+    menu.style.display = 'none'
+  }
+})
