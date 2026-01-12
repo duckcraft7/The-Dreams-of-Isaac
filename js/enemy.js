270bacec8254f32474059c5565f13b49d31799dc
duckcraft7 <duckcraft210@gmail.com>
2026-01-12 17:17:48 -0300

    Modify enemy movement towards godRig instead of player

--- js/enemy.js
+++ js/enemy.js
@@ -1,13 +1,13 @@
 AFRAME.registerComponent('enemy', {
   tick: function (time, delta) {
-    const player = document.querySelector('#player').object3D.position
+    const godPos = document.querySelector('#godRig').object3D.position
     const pos = this.el.object3D.position
 
     const dir = new THREE.Vector3()
-    dir.subVectors(player, pos)
+    dir.subVectors(godPos, pos)
     dir.y = 0
     dir.normalize()
 
-    pos.addScaledVector(dir, delta * 0.0008)
+    pos.addScaledVector(dir, delta * 0.0006)
   }
 })
